958eae99faff9d58d3dfcb0581f249080fdf5ed3
Ricardo <rsribeiro@users.noreply.github.com>
2022-08-18 21:09:27 -0300

    Refactor debug commands

--- src/network/connection/debug.rs
+++ src/network/connection/debug.rs
@@ -28,89 +28,14 @@ impl Connection {
     async fn debug_command(&self, command: &str, args: Vec<&str>) -> Result<()> {
         log::debug!("Received debug command {:?}", command);
         match command {
-            "chars" => {
-                let chars: Vec<u8> = (0x20_u8..=0x7f).collect();
-                let chat_msg = unsafe { std::str::from_utf8_unchecked(&chars) };
-
-                let message = if self.protocol < Protocol::Tibia501 {
-                    self.prepare_chat(
-                        ChatType::Normal,
-                        chat_msg,
-                        Some(&self.player),
-                        Some(self.player.position),
-                    )
-                    .await?
-                } else {
-                    self.prepare_chat(
-                        ChatType::RedConsoleWhiteScreen,
-                        chat_msg,
-                        None,
-                        Some(self.player.position),
-                    )
-                    .await?
-                };
-
-                self.queue_message(message).await;
-                Ok(())
-            }
-            "char" => {
-                let character = u8::from_str_radix(args[0], 16)?;
-                let chat_msg = format!("0x{:02x?}={}", character, character as char);
-
-                let message = if self.protocol < Protocol::Tibia501 {
-                    self.prepare_chat(
-                        ChatType::Normal,
-                        &chat_msg,
-                        Some(&self.player),
-                        Some(self.player.position),
-                    )
-                    .await?
-                } else {
-                    self.prepare_chat(
-                        ChatType::RedConsoleWhiteScreen,
-                        &chat_msg,
-                        None,
-                        Some(self.player.position),
-                    )
-                    .await?
-                };
-
-                self.queue_message(message).await;
-                Ok(())
-            }
-            "echo" => {
-                let mut buf = Cursor::new(Vec::<u8>::new());
-                buf.write_header(HeaderSend::Echo, self.protocol).await?;
-                self.queue_message(buf.into_inner()).await;
-                Ok(())
-            }
-            "item" => {
-                let slot: InventorySlot = args[0].parse::<u8>()?.try_into()?;
-                let item = u16::from_str_radix(args[1], 16)?;
-                log::trace!("Giving item 0x{:04x?} on slot {:?}", item, slot);
-                self.queue_message(self.prepare_equipped_item(slot, item, 0).await?)
-                    .await;
-                Ok(())
-            }
-            "i" => {
-                let slot = InventorySlot::RightHand;
-                let item = u16::from_str_radix(args[0], 16)?;
-                log::trace!("Giving item 0x{:04x?} on slot {:?}", item, slot);
-                self.queue_message(self.prepare_equipped_item(slot, item, 0).await?)
-                    .await;
-                Ok(())
-            }
+            "chars" => self.command_chars().await,
+            "char" => self.command_char(args[0]).await,
+            "echo" => self.command_echo().await,
+            "item" => self.command_item(args[0], args[1]).await,
+            "i" => self.command_item_right_hand(args[0]).await,
             "stats" => Ok(self.queue_message(self.prepare_stats().await?).await),
             "skills" => Ok(self.queue_message(self.prepare_skills().await?).await),
-            "me" => {
-                let effect = args[0].parse::<u8>()?.try_into()?;
-                self.queue_message(
-                    self.prepare_magic_effect(effect, self.player.position)
-                        .await?,
-                )
-                .await;
-                Ok(())
-            }
+            "me" => self.command_magic_effect(args[0]).await,
             "wlight" => Ok(self
                 .queue_message(self.prepare_world_light(args[0].parse::<u8>()?).await?)
                 .await),
@@ -134,76 +59,15 @@ impl Connection {
             "error" => Ok(self
                 .queue_message(self.prepare_error(&args.join(" ")).await?)
                 .await),
-            "motd" => {
-                static NEXT_MOTD: AtomicU16 = AtomicU16::new(0x0102);
-                let message_number = NEXT_MOTD.fetch_add(1, Ordering::SeqCst);
-
-                Ok(self
-                    .queue_message(
-                        self.prepare_message_of_the_day(message_number, &args.join(" "))
-                            .await?,
-                    )
-                    .await)
-            }
+            "motd" => self.command_motd(args).await,
             "status" => Ok(self
                 .queue_message(self.prepare_status_message(&args.join(" ")).await?)
                 .await),
             "panic" => panic!("{}", args.join(" ")),
-            "chat" => {
-                static NEXT_TYPE: AtomicU8 = AtomicU8::new(0x41);
-                let chat_type = NEXT_TYPE.fetch_add(1, Ordering::SeqCst).try_into()?;
-
-                let msg = &format!("chat_type=0x{:02x?}", chat_type);
-                log::trace!("{}", msg);
-
-                self.queue_message(
-                    self.prepare_chat(
-                        chat_type,
-                        msg,
-                        Some(&self.player),
-                        Some(self.player.position),
-                    )
-                    .await?,
-                )
-                .await;
-                Ok(())
-            }
-            "outfit" => {
-                let outfit = args[0].parse::<u8>()?.try_into()?;
-                let outfit_colors = Outfit::new(0, 0, 0, 0);
-                self.queue_message(
-                    self.prepare_update_outfit(self.player_id, outfit, outfit_colors)
-                        .await?,
-                )
-                .await;
-                Ok(())
-            }
-            "cd" => {
-                let direction = args[0].parse::<u8>()?.try_into()?;
-
-                let mut msg = self
-                    .prepare_update_object(
-                        self.player.position,
-                        crate::constants::ObjectUpdateType::Update,
-                        1,
-                    )
-                    .await?;
-                msg.extend(
-                    self.prepare_change_direction(self.player.id, direction)
-                        .await?,
-                );
-                self.queue_message(msg).await;
-                Ok(())
-            }
-            "gc" => {
-                let chat_msg = args.join(" ");
-                self.queue_message(
-                    self.prepare_green_chat(ChatType::Normal, &chat_msg, None, None)
-                        .await?,
-                )
-                .await;
-                Ok(())
-            }
+            "chat" => self.command_chat().await,
+            "outfit" => self.command_outfit(args[0]).await,
+            "cd" => self.command_change_direction(args[0]).await,
+            "gc" => self.command_green_chat(args).await,
             "u0" => Ok(self
                 .queue_message(self.prepare_unknown_0x0000().await?)
                 .await),
@@ -223,5 +87,162 @@ impl Connection {
                 )
                 .await),
         }
+    }
+
+    async fn command_chars(&self) -> Result<()> {
+        let chars: Vec<u8> = (0x20_u8..=0x7f).collect();
+        let chat_msg = unsafe { std::str::from_utf8_unchecked(&chars) };
+
+        let message = if self.protocol < Protocol::Tibia501 {
+            self.prepare_chat(
+                ChatType::Normal,
+                chat_msg,
+                Some(&self.player),
+                Some(self.player.position),
+            )
+            .await?
+        } else {
+            self.prepare_chat(
+                ChatType::RedConsoleWhiteScreen,
+                chat_msg,
+                None,
+                Some(self.player.position),
+            )
+            .await?
+        };
+
+        self.queue_message(message).await;
+        Ok(())
+    }
+
+    async fn command_char(&self, arg: &str) -> Result<()> {
+        let character = u8::from_str_radix(arg, 16)?;
+        let chat_msg = format!("0x{:02x?}={}", character, character as char);
+
+        let message = if self.protocol < Protocol::Tibia501 {
+            self.prepare_chat(
+                ChatType::Normal,
+                &chat_msg,
+                Some(&self.player),
+                Some(self.player.position),
+            )
+            .await?
+        } else {
+            self.prepare_chat(
+                ChatType::RedConsoleWhiteScreen,
+                &chat_msg,
+                None,
+                Some(self.player.position),
+            )
+            .await?
+        };
+
+        self.queue_message(message).await;
+        Ok(())
+    }
+
+    async fn command_echo(&self) -> Result<()> {
+        let mut buf = Cursor::new(Vec::<u8>::new());
+        buf.write_header(HeaderSend::Echo, self.protocol).await?;
+        self.queue_message(buf.into_inner()).await;
+        Ok(())
+    }
+
+    async fn command_item(&self, slot: &str, item: &str) -> Result<()> {
+        let slot: InventorySlot = slot.parse::<u8>()?.try_into()?;
+        let item = u16::from_str_radix(item, 16)?;
+        log::trace!("Giving item 0x{:04x?} on slot {:?}", item, slot);
+        self.queue_message(self.prepare_equipped_item(slot, item, 0).await?)
+            .await;
+        Ok(())
+    }
+
+    async fn command_item_right_hand(&self, item: &str) -> Result<()> {
+        let slot = InventorySlot::RightHand;
+        let item = u16::from_str_radix(item, 16)?;
+        log::trace!("Giving item 0x{:04x?} on slot {:?}", item, slot);
+        self.queue_message(self.prepare_equipped_item(slot, item, 0).await?)
+            .await;
+        Ok(())
+    }
+
+    async fn command_magic_effect(&self, effect: &str) -> Result<()> {
+        let effect = effect.parse::<u8>()?.try_into()?;
+        self.queue_message(
+            self.prepare_magic_effect(effect, self.player.position)
+                .await?,
+        )
+        .await;
+        Ok(())
+    }
+
+    async fn command_motd(&self, args: Vec<&str>) -> Result<()> {
+        static NEXT_MOTD: AtomicU16 = AtomicU16::new(0x0102);
+        let message_number = NEXT_MOTD.fetch_add(1, Ordering::SeqCst);
+        self.queue_message(
+            self.prepare_message_of_the_day(message_number, &args.join(" "))
+                .await?,
+        )
+        .await;
+        Ok(())
+    }
+
+    async fn command_chat(&self) -> Result<()> {
+        static NEXT_TYPE: AtomicU8 = AtomicU8::new(0x41);
+        let chat_type = NEXT_TYPE.fetch_add(1, Ordering::SeqCst).try_into()?;
+
+        let msg = &format!("chat_type=0x{:02x?}", chat_type);
+        log::trace!("{}", msg);
+
+        self.queue_message(
+            self.prepare_chat(
+                chat_type,
+                msg,
+                Some(&self.player),
+                Some(self.player.position),
+            )
+            .await?,
+        )
+        .await;
+        Ok(())
+    }
+
+    async fn command_outfit(&self, outfit: &str) -> Result<()> {
+        let outfit = outfit.parse::<u8>()?.try_into()?;
+        let outfit_colors = Outfit::new(0, 0, 0, 0);
+        self.queue_message(
+            self.prepare_update_outfit(self.player_id, outfit, outfit_colors)
+                .await?,
+        )
+        .await;
+        Ok(())
+    }
+
+    async fn command_change_direction(&self, direction: &str) -> Result<()> {
+        let direction = direction.parse::<u8>()?.try_into()?;
+
+        let mut msg = self
+            .prepare_update_object(
+                self.player.position,
+                crate::constants::ObjectUpdateType::Update,
+                1,
+            )
+            .await?;
+        msg.extend(
+            self.prepare_change_direction(self.player.id, direction)
+                .await?,
+        );
+        self.queue_message(msg).await;
+        Ok(())
+    }
+
+    async fn command_green_chat(&self, args: Vec<&str>) -> Result<()> {
+        let chat_msg = args.join(" ");
+        self.queue_message(
+            self.prepare_green_chat(ChatType::Normal, &chat_msg, None, None)
+                .await?,
+        )
+        .await;
+        Ok(())
     }
 }
